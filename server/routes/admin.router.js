b40247a80c7453d94fd4dfe64085e2696b1c4eba
carlbarfuss <carlbarfuss@gmail.com>
2021-02-03 13:48:40 -0600

    sample data for company table to database.sql

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -5,7 +5,27 @@ const router = express.Router();
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/company', (req, res) => {
+   const sqlText = `SELECT * FROM "company"`
+   pool.query(sqlText, [])
+   .then( (result) => {
+      res.send(result.rows)
+   })
+   .catch( (error) => {
+      console.log('error in COMPANY GET route, ', error);
+      res.sendStatus(500)
+   })
+});
+
+router.get('/user', (req, res) => {
+   // GET route code here
+});
+
+router.get('/location', (req, res) => {
+   // GET route code here
+});
+
+router.get('/project', (req, res) => {
    // GET route code here
 });
 
