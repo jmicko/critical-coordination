b7d83a7591b99cb002af754ced4a1e22efe5fe04
carlbarfuss <carlbarfuss@gmail.com>
2021-02-05 10:04:46 -0600

    added new company form to admin page

--- src/components/AdminCompany/AdminCompany.js
+++ src/components/AdminCompany/AdminCompany.js
@@ -9,36 +9,76 @@ class AdminCompany extends Component {
 
   async componentDidMount() {
     // Get's data to populate lists/tables
-    this.props.dispatch({type: 'FETCH_ALLCOMPANY'});
-}
-
-  state = {
-    stateBuffer: 0,
+    this.props.dispatch({ type: 'FETCH_ALLCOMPANY' });
   }
 
+  state = {
+    newCompany: {
+      company: '',
+    }
+  };
+
+  handleChange = (event, type) => {
+    this.setState({
+      newCompany: {
+        ...this.state.newCompany,
+        [type]: event.target.value
+      }
+    })
+  }
+
+  addCompany = (event) => {
+    event.preventDefault();
+    let company = this.state.newCompany;
+    if (company.company !== '') {
+      console.log('in addCompany');
+      this.props.dispatch({ type: 'ADMIN_ADD_COMPANY', payload: company })
+    } else {
+      alert('Please ensure all fields are filled before submitting')
+    }
+    this.setState({
+      showAddUser: false,
+      newCompany: {
+        company: '',
+      }
+    })
+  }
+
+  showAddCompany = () => {
+    this.setState({
+      showAddCompany: !this.state.showAddCompany
+    })
+    console.log('in addCompany');
+  }
 
   render() {
     return (
       <div>
-        <h3>Admin <span style={{textDecoration: "underline"}}>Company</span> Page, {this.props.store.user.first_name}!</h3>
-
-              <button>Add Company</button>
-              <table className="tableClass">
-                <thead className="headerClass">
-                  <tr><th>Company Name</th><th>&nbsp;</th><th>&nbsp;</th></tr>
-                </thead>
-                <tbody className="bodyClass">
-                    {this.props.store.admin.allCompanyReducer.map((lineItem, index) => {
-                        return (
-                          <tr key={index}>
-                              <td>{lineItem.company_name}</td>
-                              <td><button className="adminButtonClass">Modify</button></td>
-                              <td><button className="adminButtonClass">Delete</button></td>
-                          </tr>
-                        );
-                    })} 
-                </tbody>
-              </table>
+        <h3>Admin Company Page</h3>
+        {this.state.showAddCompany ?
+          <> <h4>Add New User</h4>
+            <form>
+              <label>New Company Name: </label>
+              <input required onChange={(event) => this.handleChange(event, 'company')} value={this.state.newCompany.company}></input>
+              <button className="button" type="submit" onClick={(event) => this.addCompany(event)}>Add Company</button>
+            </form><button onClick={this.showAddCompany}>Close</button></>
+          : <button onClick={this.showAddCompany}>Add Company</button>}
+        <table className="tableClass">
+          <thead className="headerClass">
+            <tr><th>Company Name</th><th>&nbsp;</th><th>&nbsp;</th></tr>
+          </thead>
+          <tbody className="bodyClass">
+            {this.props.store.admin.allCompanyReducer.map((lineItem, index) => {
+              return (
+                <tr key={index}>
+                  <td>{lineItem.company_name}</td>
+                  <td><button className="adminButtonClass">Modify</button></td>
+                  <td><button className="adminButtonClass">Delete</button></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
