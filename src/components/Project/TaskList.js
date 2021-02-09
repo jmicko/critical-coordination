a6752dc92edc4a4f0f07371ca856575e7448683d
BPJ94487 <BPJ94487@bethel.edu>
2021-02-09 11:53:06 -0600

    the tasks are rendering and the tasks edit button is rendering conditionally as well

--- src/components/Project/TaskList.js
+++ src/components/Project/TaskList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+import EditButton from './TaskListPopUp'
+
 const getCookie = (cookieName) => {
     // Get name followed by anything except a semicolon
     const cookieString = RegExp('' + cookieName + '[^;]+').exec(document.cookie);
@@ -14,14 +16,26 @@ class TaskList extends Component {
         this.props.dispatch({ type: 'FETCH_PROJECT_TASKS', payload: getCookie('project') })
     }
 
+    edit = user_type => {
+        if (user_type !== "client") {
+            return <EditButton/>
+        }
+    }
+
   render() {
     return (      
         <div className="container paper">
             <h3>Task List</h3>
             {this.props.store.projectReducer.projectTaskReducer.map((task) => {
-                return <div key={task.id}>
-                        <p> {task.task_name}</p>
+                return <div key={task.id} onClick={ () => this.navigate}>
+                        <p>Task: {task.task_name} </p>
+                        <p>Date Scheduled: {task.scheduled_date} </p>
+                        <p>NLT Date: {task.nlt_date} </p>                     
+
+                        {this.edit(this.props.store.user.user_type)}
+                        
                         </div>
+                    
             })}
 
                                         
