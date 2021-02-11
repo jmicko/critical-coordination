20037cb18d9822edb15c5ed0c27fe064bc07f3db
John Micko <jrmicko5012@gmail.com>
2021-02-11 14:22:59 -0600

    Condense render logic

--- src/components/AdminPage/AdminPage.js
+++ src/components/AdminPage/AdminPage.js
@@ -10,91 +10,32 @@ import AddNewProject from '../AddNewProject/AddNewProject'
 
 class AdminPage extends Component {
 
- componentDidMount() {
-}
+  componentDidMount() {
+  }
 
   state = {
-    showUser: false,
-    showCompany: false, 
-    showLocation: false,
-    showTaskStatus: false,
-    showNewProject: false,
-  }
-
-  showUser = () => {
-      this.setState ({
-        showUser: !this.state.showUser,
-        showCompany: false, 
-        showLocation: false,
-        showTaskStatus: false,
-        showNewProject: false,
-  })
-  } 
-
-  showCompany = () => {
-    this.setState ({
-      showUser: false,
-      showCompany: !this.state.showCompany, 
-      showLocation: false,
-      showTaskStatus: false,
-      showNewProject: false,
-    })
-  }
-
-  showLocation = () => {
-    this.setState ({
-      showUser: false,
-      showCompany: false, 
-      showLocation: !this.state.showLocation,
-      showTaskStatus: false,
-      showNewProject: false,
-    })
-  }
-  
-  showTaskStatus = () => {
-    this.setState ({
-      showUser: false,
-      showCompany: false, 
-      showLocation: false,
-      showTaskStatus: !this.state.showTaskStatus,
-      showNewProject: false,
-    })
-  }
-
-  showNewProject = () => {
-    this.setState ({
-      showUser: false,
-      showCompany: false,
-      showLocation: false,
-      showTaskStatus: false,
-      showNewProject: !this.state.showNewProject,
-    })
+    active: "user"
   }
 
   render() {
-    if(this.props.store.user.user_type === 'admin'){
+    if (this.props.store.user.user_type === 'admin') {
       return (
         <center>
-          <h1>Admin Page</h1>  
+          <h1>Admin Page</h1>
           {/* {JSON.stringify(this.props)} */}
-          <button className="adminButtonClass" onClick={()=>this.showUser()}>Manage User</button>
-          <button className="adminButtonClass" onClick={()=>this.showCompany()}>Manage Company</button>
-          <button className="adminButtonClass" onClick={()=>this.showTaskStatus()}>Manage Task Status</button>
-          <br/><br/>
-          {this.state.showNewProject ?
-          <AddNewProject /> : <p></p>}
-          {/* <h3>User List</h3> */}
-          {this.state.showUser ?
-          <AdminUser />: <p></p>}
-          {/* <h3>Company List</h3> */}
-          {this.state.showCompany ?
-          <AdminCompany />: <p></p>}
-          {/* <h3>Location List</h3> */}
-          {this.state.showCompany ?
-          <AdminLocation />: <p></p>}
-          {/* <h3>Category List</h3> */}
-          {this.state.showTaskStatus ?
-          <AdminTaskStatus />: <p></p>}
+          <button className="btn" onClick={() => this.setState({ active: "user" })}>Manage User</button>
+          <button className="btn" onClick={() => this.setState({ active: "company" })}>Manage Company</button>
+          <button className="btn" onClick={() => this.setState({ active: "task" })}>Manage Task Status</button>
+          <br /><br />
+          {this.state.active === "user" &&
+            <AdminUser />}
+          {this.state.active === "company" &&
+            <>
+              <AdminCompany />
+              <AdminLocation />
+            </>}
+          {this.state.active === "task" &&
+            <AdminTaskStatus />}
         </center>
       );
     } else {
