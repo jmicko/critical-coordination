93543aa2a767947d7c1ce7f70aabe80ac4e8c384
John Micko <jrmicko5012@gmail.com>
2021-02-15 16:22:35 -0600

    Admin page and header style

--- src/components/AddUserForm/AddUserForm.js
+++ src/components/AddUserForm/AddUserForm.js
@@ -31,13 +31,13 @@ class AddUserForm extends Component {
    addUser = (event) => {
       event.preventDefault();
       let user = this.state.newUser;
-      if(user.email !== '' && user.company !== '' 
-      && user.first_name !== '' && user.last_name !== '' 
-      && user.password !== '' && user.user_type !== '') {
+      if (user.email !== '' && user.company !== ''
+         && user.first_name !== '' && user.last_name !== ''
+         && user.password !== '' && user.user_type !== '') {
          console.log('in addUser');
-         this.props.dispatch( {type: 'ADMIN_ADD_USER', payload: user})
+         this.props.dispatch({ type: 'ADMIN_ADD_USER', payload: user })
       } else {
-         alert ('Please ensure all fields are filled before submitting')
+         alert('Please ensure all fields are filled before submitting')
       }
       this.setState({
          newUser: {
@@ -54,24 +54,48 @@ class AddUserForm extends Component {
    render() {
       return (
          <div>
-            <h4>Add New User</h4>
-            <form>
-               <input required placeholder="email" onChange={(event) => this.handleChange(event, 'email')} value={this.state.newUser.email}></input>
-               <select required onChange={(event) => this.handleChange(event, 'company')} value={this.state.newUser.company}>
-                  {this.props.store.admin.allCompanyReducer.map( (company) => {
+            <form className="formPanel metal">
+               <div className="highlighter">
+                  <h2>Add New User</h2>
+               </div>
+               <input
+                  required
+                  placeholder="email" onChange={(event) => this.handleChange(event, 'email')}
+                  value={this.state.newUser.email}></input>
+               <select
+                  required
+                  onChange={(event) => this.handleChange(event, 'company')}
+                  value={this.state.newUser.company}>
+                  {this.props.store.admin.allCompanyReducer.map((company) => {
                      return (
-                        <option key={company.id} value={company.id}>Company: {company.company_name}</option>)})}
+                        <option key={company.id} value={company.id}>Company: {company.company_name}</option>)
+                  })}
                </select>
-               <input required placeholder="First Name" onChange={(event) => this.handleChange(event, 'first_name')} value={this.state.newUser.first_name}></input>
-               <input required placeholder="Last Name" onChange={(event) => this.handleChange(event, 'last_name')} value={this.state.newUser.last_name}></input>
-               <input required placeholder="password" onChange={(event) => this.handleChange(event, 'password')} value={this.state.newUser.password}></input>
+               <input
+                  required
+                  placeholder="First Name"
+                  onChange={(event) => this.handleChange(event, 'first_name')}
+                  value={this.state.newUser.first_name}></input>
+               <input
+                  required
+                  placeholder="Last Name"
+                  onChange={(event) => this.handleChange(event, 'last_name')}
+                  value={this.state.newUser.last_name}></input>
+               <input
+                  required
+                  placeholder="password"
+                  onChange={(event) => this.handleChange(event, 'password')}
+                  value={this.state.newUser.password}></input>
                <select required placeholder="user type" onChange={(event) => this.handleChange(event, 'user_type')} value={this.state.newUser.user_type}>
                   <option value="">User Type: </option>
                   <option value="client">User Type: Client</option>
                   <option value="contractor">User Type: Contractor</option>
                   <option value="admin">User Type: Admin</option>
                </select>
-               <button className="button" type="submit" onClick={(event) => this.addUser(event)}>Add User</button>
+               <button
+                  className="button"
+                  type="submit"
+                  onClick={(event) => this.addUser(event)}>Add User</button>
             </form>
          </div>
       );
