441b1cb946cc29aa5ef8354e00f90e7d23f8556a
John Micko <jrmicko5012@gmail.com>
2021-02-14 23:45:17 -0600

    notes

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -9,33 +9,49 @@ router.get('/:id', rejectUnauthenticated, (req, res) => {
   console.log(`SHOULD BE task id from task router line 7`, req.params);
   const sqlText = `SELECT * FROM "task" WHERE "id"=$1`
   pool.query(sqlText, [req.params.id])
-  .then( (result) => {
-    console.log(`hey look at these RESULTS`, result.rows);
-     res.send(...result.rows)
-  })
-  .catch( (error) => {
-     console.log('error in TASK GET route, ', error);
-  })
+    .then((result) => {
+      console.log(`hey look at these RESULTS`, result.rows);
+      res.send(...result.rows)
+    })
+    .catch((error) => {
+      console.log('error in TASK GET route, ', error);
+    })
 });
 
 
 // TODO - THIS SHOULD GO IN PROJECT ROUTER
+// or wait, it's right? Wow wish I had notes
 router.get('/project/:id', rejectUnauthenticated, (req, res) => {
   const task = req.params.id;
-  const sqlText = `SELECT task.id AS id, company_name, nlt_date, poc_fk, project_fk, scheduled_date, status_type, task_name, task_name_fk, task_status_fk, technician_info, token, tracking_id, updated_by, notes, company_fk, notified_date  
-                  FROM task 
-                  JOIN task_name ON task_name.id = task.task_name_fk
-                  JOIN task_status ON task_status.id = task.task_status_fk
-                  JOIN company on company.id = task.company_fk
-                  where project_fk = $1 
-                  ORDER BY nlt_date ASC;`;
+  const sqlText = `SELECT task.id AS id,
+                          company_name,
+                          nlt_date,
+                          poc_fk,
+                          project_fk,
+                          scheduled_date,
+                          status_type, task_name,
+                          task_name_fk,
+                          task_status_fk,
+                          technician_info,
+                          token,
+                          tracking_id,
+                          updated_by,
+                          notes,
+                          company_fk,
+                          notified_date  
+    FROM task 
+    JOIN task_name ON task_name.id = task.task_name_fk
+    JOIN task_status ON task_status.id = task.task_status_fk
+    JOIN company on company.id = task.company_fk
+    where project_fk = $1 
+    ORDER BY nlt_date ASC;`;
   pool.query(sqlText, [task])
-  .then( (result) => {
-    res.send(result.rows)
-  })
-  .catch ( (error) => {
-    console.log('error in getting tasks for Project', error);
-  })
+    .then((result) => {
+      res.send(result.rows)
+    })
+    .catch((error) => {
+      console.log('error in getting tasks for Project', error);
+    })
 })
 
 // TODO - THIS SHOULD GO INTO IT'S OWN MODULE
@@ -59,9 +75,9 @@ router.put('/update', rejectUnauthenticated, (req, res) => {
       res.sendStatus(201);
     })
     .catch((error) => {
-    console.log('Error completeing the TASK UPDATE in task.router.js', error);
-    res.sendStatus(500);  
-  });
+      console.log('Error completeing the TASK UPDATE in task.router.js', error);
+      res.sendStatus(500);
+    });
 });
 
 // this does not protect the contractor route because it does not check the user type
@@ -73,9 +89,9 @@ router.put('/contractor', rejectUnauthenticated, (req, res) => {
       res.sendStatus(201);
     })
     .catch((error) => {
-    console.log('Error completeing the CONTRACTOR TASK UPDATE in task.router.js', error);
-    res.sendStatus(500);  
-  });
+      console.log('Error completeing the CONTRACTOR TASK UPDATE in task.router.js', error);
+      res.sendStatus(500);
+    });
 });
 
 
@@ -88,9 +104,9 @@ router.put('/delete', rejectUnauthenticated, (req, res) => {
       res.sendStatus(201);
     })
     .catch((error) => {
-    console.log('Error completing the DELETE in task.router.js', error);
-    res.sendStatus(500);
-  });
+      console.log('Error completing the DELETE in task.router.js', error);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
