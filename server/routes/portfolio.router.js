a0d7962ba70d7673ce3664b6bb754aa03c35a7da
John Micko <jrmicko5012@gmail.com>
2021-02-17 20:23:04 -0600

    create archive get route

--- server/routes/portfolio.router.js
+++ server/routes/portfolio.router.js
@@ -53,6 +53,56 @@ router.get('/', rejectUnauthenticated, (req, res) => {
     }
 });
 
+// ---- GET ARCHIVED route ----
+router.get('/archive', rejectUnauthenticated, (req, res) => {  
+
+    //This will grab everything for anyone checking as an administrator
+    // and the else is for any other company other than critical coordination
+    if( req.user.user_type === 'admin' ) {
+        const queryTextAdmin = `SELECT project_name, project.id, due_date, "PO_Number", address, location_name, location_fk  FROM project
+                                JOIN company_location ON company_location.id = project.location_fk
+                                JOIN company ON company.id = project.company_fk
+                                WHERE project.archived=true
+                                ORDER BY project.due_date ASC;`;
+        pool.query(queryTextAdmin)
+            .then((result) => {
+                res.send(result.rows);
+            })
+            .catch((error) => {
+                console.log('Error completing the GET route for PORTFOLIO ADMIN', error);
+                res.sendStatus(500);
+            });
+    } else if (req.user.user_type === 'client') {
+        const queryText1 =` SELECT project_name, project.id, due_date, "PO_Number", address, location_name, location_fk  FROM project
+                            JOIN company_location ON company_location.id = project.location_fk
+                            JOIN company ON company.id = project.company_fk
+                            WHERE project.company_fk = $1 and project.archived=true
+                            ORDER BY project.due_date ASC;`;    
+        pool.query(queryText1, [req.user.company_fk])
+            .then((result) => {
+                res.send(result.rows);
+            })
+            .catch((error) => {
+                console.log('Error completing the GET route for PORTFOLIO', error);
+                res.sendStatus(500);
+            });
+    } else if (req.user.user_type === 'contractor') {
+        const queryText1 = `SELECT * FROM task
+                            JOIN company on company.id = task.company_fk
+                            JOIN project on project.id = task.project_fk
+                            WHERE task.company_fk = $1 and task.archived=true
+                            ORDER BY task.nlt_date ASC;`
+        pool.query(queryText1, [req.user.company_fk])
+            .then((result) => {
+                res.send(result.rows);
+            })
+            .catch((error) => {
+                console.log('Error completing the GET route for PORTFOLIO', error);
+                res.sendStatus(500);
+            });
+    }
+});
+
 
 router.put('/update', rejectUnauthenticated, (req, res) => {
     const queryText1 =  `UPDATE project SET project_name=$1, "PO_Number"=$2, due_date=$3 WHERE id = $4;`;
